1ee8dc878e5462ee32e4d69c361bf68e587521d5
514736 <514736@LT059063.cts.com>
2018-01-16 20:55:18 +0300

    test commit one

---
+++ src/test/java/testtest.java
@@ -0,0 +1,16 @@
+
+public class testtest {
+
+	public static void main(String[] args) {
+		String s="The Service Request 3-967293042 has been success created";
+		if (s.contains("success")) {
+			System.out.println(s.substring(0,56));
+		}
+		else
+			System.out.println("Duplicate");
+		//git commit test
+		
+
+	}
+
+}
